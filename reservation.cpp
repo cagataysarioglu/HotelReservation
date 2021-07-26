631fe0d1e3413aa7a16775f857e0ccd55dbc3780
Çağatay Sarıoğlu <69264926+cagataysarioglu@users.noreply.github.com>
2021-07-26 13:15:07 +0300

    Update reservation.cpp

--- reservation.cpp
+++ reservation.cpp
@@ -20,7 +20,54 @@ class Otel
   void OdaRezervasyonu();
   void OdaBosaltma();
   void MusteriSayisi();
-  void YatakOdasiRaporu();
-  void SeminerOdasiRaporu();
+  void YatakOdasiSorgu();
+  void SeminerOdasiSorgu();
   void Gelir();
 };
+
+Otel::Otel()
+{
+  SayacBir = 0;
+  SayacIki = 0;
+  Toplam = 0;
+  
+  OdaIsletmesi();
+}
+
+void Otel::OdaIsletmesi()
+{
+  for (int i=1; i<46; i++)
+    Odalar[i] = Bos;
+  
+  int Sayi;
+  
+  do{
+    cout<<"\n\t\t--- Altıntepe Oteli ---"<<endl;
+    cout<<"\t\t1. Oda Rezervasyonu"<<endl;
+    cout<<"\t\t2. Oda Boşaltma"<<endl;
+    cout<<"\t\t3. Dolu Oda Sayısı"<<endl;
+    cout<<"\t\t4. Yatak Odası Sorgula"<<endl;
+    cout<<"\t\t5. Seminer Odası Sorgula"<<endl;
+    cout<<"\t\t6. Günlük Toplam Gelir"<<endl;
+    cout<<"\t\t7. Çıkış Yap"<<endl;
+    cout<<"Seçiminizin sayısını giriniz: ";
+    cin>>Sayi;
+    
+    switch(Sayi){
+      case 1: OdaRezervasyonu();
+        break;
+      case 2: OdaBosaltma();
+        break;
+      case 3: MusteriSayisi();
+        break;
+      case 4: YatakOdasiSorgu();
+        break;
+      case 5: SeminerOdasiSorgu();
+        break;
+      case 6: Gelir();
+        break;
+      case 7: exit(1);
+      default: cout<<"\nYanlış bir sayı girildi. Yeniden deneyiniz: "<<endl;
+    }
+  } while(Sayi!=7);
+}
